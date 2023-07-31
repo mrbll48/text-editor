eaef7e19eca6bb659a3ce6146b45a215a185da9a
mrbll48 <william.marks87@gmail.com>
2023-07-31 16:39:05 -0700

    basic structure created, not functional at this time

---
+++ client/src/js/database.js
@@ -0,0 +1,47 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log("Post to database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.add({ content });
+  const result = await request;
+  if (result) {
+    console.log("data saved to database", result);
+  } else {
+    console.error("putDb not implemented");
+  }
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("Get from database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+
+  if (result) {
+    console.log("result.value", result);
+    return result;
+  } else {
+    console.error("getDb not implemented");
+  }
+};
+
+initdb();
